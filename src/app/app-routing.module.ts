772f54dbc7a5468616832c7a9919cb10db2d6a7f
Nhi Luong <nhi.luong@mavs.uta.edu>
2020-11-14 03:14:57 -0600

    implemet guard to restrict user access certain route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CartComponent } from './cart/cart.component';
+import { AuthGuard } from './guard/auth.guard';
 import { HomepageComponent } from './homepage/homepage.component';
 import { LoginComponent } from './login/login.component';
 import { ProductComponent } from './product/product.component';
@@ -11,9 +12,9 @@ const routes: Routes = [
   {path: 'homepage', component: HomepageComponent},
   {path: 'login',component:LoginComponent},
   {path: 'user-registration', component: UserRegistrationComponent},
-  {path: 'manage-products', component: ProductComponent},
-  {path: 'manage-users', component: UserComponent},
-  {path: 'cart', component: CartComponent}
+  {path: 'manage-products', component: ProductComponent, canActivate: [AuthGuard]},
+  {path: 'manage-users', component: UserComponent, canActivate: [AuthGuard]},
+  {path: 'cart', component: CartComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({
--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -1,15 +1,35 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { LoginService } from '../service/login.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+  constructor(private loginService: LoginService){}
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+      console.log(route)
+      console.log(state)
+      if (this.loginService.isLogin){
+        if ((state.url =="/manage-users")||(state.url == "/manage-products")){
+          if (this.loginService.currentUser.usertype!= 'admin'){
+            alert("You need to be Admin to visit this route")
+            return false
+          } 
+        }
+        if (state.url == "/cart"){
+          if (this.loginService.currentUser.usertype!= 'customer'){
+            alert("You need to be Customer to visit cart route")
+            return false
+          } 
+        }
+        return true;
+      }
+      alert("You don't have permission to view this page, please Login first")
+      return false;
   }
   
 }
